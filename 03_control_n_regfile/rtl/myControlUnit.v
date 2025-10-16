e7cf6d333fb07c4ba9d11ba57bc36c6e76b3114d
Nick Archi <nicholas.archibong@gmail.com>
2025-10-16 13:59:48 -0500

    Starting implementation of ControlUnit

--- 03_control_n_regfile/rtl/myControlUnit.v
+++ 03_control_n_regfile/rtl/myControlUnit.v
@@ -1,3 +1,31 @@
-module myControlUnit();
+/*
+* Description:
+* [] This file is intended to be a control unit that would dissect an N-bit opcode to produce the 
+* necessary control signals for the rest of the computer datapath.
+*
+* Objective
+* [] generate control signals from N-bit instruction
+*/
+module myControlUnit #(parameter OPCODELEN = 7, parameter ALUOPLEN = 3)
+(
+	opcode,		// N-bit instruction opcode
+	branch,		// branch or not
+	memRea,		// memory read
+	memToReg, 	// memory to register
+	aluOp,		// N-bit operation for ALU
+	memWri,		// memory write
+	aluSrc,		// operand for ALU from registers or not
+	regWri		// perform write to register or not
+);
+
+	input[OPCODELEN-1:0] opcode;
+	
+	output branch, memRea, memToReg, memWri, aluSrc, regWri;
+	output[ALUOPLEN-1:0] aluOp;
+
+	// combinational piece of logic...
+	always@(*) begin
+		// [TODO] implement case statements to check instruction and output necessary signals
+	end
 
 endmodule
--- 03_control_n_regfile/rtl/myRegFile.v
+++ 03_control_n_regfile/rtl/myRegFile.v
@@ -1,3 +1,12 @@
+/*
+* Description:
+* [] This file is intended to be the implementation of a register file that would provide
+* operations to read and write values to essentially an array of memory.
+*
+* Objective
+* [] read, write operations
+* [] register 0 is special register that only returns 0
+*/
 module myRegFile #(parameter WIDTH = 32, parameter NUMOFREGS = 32)
 (
 	clk,
